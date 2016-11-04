1b3125692276fbb02940666d2c5630b8e95d9fd0
jessgriffin <jessica.griffin@ocsbstudent.ca>
2016-11-04 14:41:00 -0400

    Update MagicSquare.java

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,60 +1,93 @@
+import java.awt.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MagicSquare {
 
 	public static void main(String[] args) {
-	
-		int array[] = {1,2,3,4,5,6,7,8,9};
-		int a;
-		int b;
-		int c;
-		int d;
-		int e;
-		int f;
-		int g;
-		int h;
+		// TODO Auto-generated method stub
 		
-		for (int i = 0; i < 9; i++) {
-			
-			int j = i + 1;
-			
-			while (j < 9) {
-				
-				int tempArray[] = array;
-				
-				//we think the problem is here
-				tempArray[i] = tempArray[j];
-				tempArray[j] = array[i];
-				
-				a = tempArray[0] + tempArray[1] + tempArray[2];
-				b = tempArray[3] + tempArray[4] + tempArray[5];
-				c = tempArray[6] + tempArray[7] + tempArray[8];
-				d = tempArray[0] + tempArray[3] + tempArray[6];
-				e = tempArray[1] + tempArray[4] + tempArray[7];
-				f = tempArray[2] + tempArray[5] + tempArray[8];
-				g = tempArray[0] + tempArray[4] + tempArray[8];
-				h = tempArray[2] + tempArray[4] + tempArray[6];
-				
-				if (a == b && a == c && a == d  && a == e  && a == f  && a == g  && a == h) {
+		int tempArray[] = {1,1,1,1,1,1,1,1,1};
+		
+		for (int first = 1; first < 10; first++) {
+			for (int second = 1; second < 10; second++) {
+				for (int third = 1; third < 10; third++) {
+					for(int fourth = 1; fourth < 10; fourth++) {
+						for(int fifth = 1; fifth < 10; fifth++) {
+							for(int sixth = 1; sixth < 10; sixth++) {
+								for(int seventh = 1; seventh < 10; seventh++) {
+									for(int eighth = 1; eighth < 10; eighth++) {
+										for(int ninth = 1; ninth < 10; ninth++) {
+											tempArray[0] = first;
+											tempArray[1] = second;
+											tempArray[2] = third;
+											tempArray[3] = fourth;
+											tempArray[4] = fifth;
+											tempArray[5] = sixth;
+											tempArray[6] = seventh;
+											tempArray[7] = eighth;
+											tempArray[8] = ninth;
+											
+											int a = tempArray[0] + tempArray[1] + tempArray[2];
+											int b = tempArray[3] + tempArray[4] + tempArray[5];
+											int c = tempArray[6] + tempArray[7] + tempArray[8];
+											int d = tempArray[0] + tempArray[3] + tempArray[6];
+											int e = tempArray[1] + tempArray[4] + tempArray[7];
+											int f = tempArray[2] + tempArray[5] + tempArray[8];
+											int g = tempArray[0] + tempArray[4] + tempArray[8];
+											int h = tempArray[2] + tempArray[4] + tempArray[6];
+											
+											if (a == b && a == c && a == d  && a == e  && a == f  && a == g  && a == h) {
+												
+												Set<Integer> set = new HashSet<Integer>();
+												
+												for (int counter = 0; counter < tempArray.length; counter++) {
+													
+													set.add(tempArray[counter]);
+													
+												}
+												
+												if(set.size() < tempArray.length){
+												    /* There are duplicates */
+												} else {
+													
+													System.out.println("Magic Square!");
+													
+													System.out.print(tempArray[0]);
+													System.out.print(tempArray[1]);
+													System.out.println(tempArray[2]);
+													
+													System.out.print(tempArray[3]);
+													System.out.print(tempArray[4]);
+													System.out.println(tempArray[5]);
+													
+													System.out.print(tempArray[6]);
+													System.out.print(tempArray[7]);
+													System.out.println(tempArray[8]);
+													
+													System.out.println();
+													
+												}
+												
+											}
+											
+										}
+										
+									}
+									
+								}
+								
+							}
+							
+						}
+						
+					}
 					
-					System.out.println(tempArray[0]);
-					System.out.print(tempArray[1]);
-					System.out.print(tempArray[2]);
-					
-					System.out.println(tempArray[3]);
-					System.out.print(tempArray[4]);
-					System.out.print(tempArray[5]);
-					
-					System.out.println(tempArray[6]);
-					System.out.print(tempArray[7]);
-					System.out.print(tempArray[8]);
-					
-					System.out.println("---");
 				}
 				
-				j++; 
-				
 			}
-			
+				
 		}
 		
 	}
